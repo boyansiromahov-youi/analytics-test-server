156296ca4ac34570aa6ceb8c15c47aae5248c4e9
Luke Sanderson <luke.sanderson@youi.tv>
2019-05-28 10:43:58 -0400

    more formatting

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const path = require('path')
-const express = require('express')
-const exphbs = require('express-handlebars')
-var bodyParser     =        require("body-parser");
-const port = 3000
-const app = express()
-//import { Analytics } from "../analytics-test-server/pages/home";
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+const port = 3000;
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var analytics;
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'pages')
-}))
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'pages'))
-app.get('/', (request, response) => {
-  //document.getElementById('Analytics').setAttribute("value", "100");
-  response.render('home', {
-    name: analytics
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "pages")
   })
-})
-
-app.post('/*',function(request,response){
-  //console.log(response);
-  console.log(JSON.stringify(request.body).replace(",","<br>"));
-  analytics = JSON.stringify(request.body).split(',').join('\n');
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "pages"));
+app.get("/", (request, response) => {
+  response.render("home", {
+    name: analytics
+  });
 });
 
+app.post("/*", function(request, response) {
+  //console.log(response);
+  console.log(JSON.stringify(request.body).replace("," , "<br>"));
+  analytics = JSON.stringify(request.body)
+    .split(",")
+    .join("\n");
+});
 
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log("something bad happened", err);
   }
-  console.log(`server is listening on ${port}`)
-})
-
+  console.log(`server is listening on ${port}`);
+});
 
 /*
 const http = require("http");
