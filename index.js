d80725c55d2bb5dcd4f0b7d764bd758dc771b734
boyansiromahov-youi <46452204+boyansiromahov-youi@users.noreply.github.com>
2019-06-11 10:11:18 -0400

    Selector implementation  (#9)
    * inital commmit
    
    * formating and comments
    
    * made the selector dissapear when paused

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 var analytics;
-var array = [];
+var n = 0;
+var callsDict = {};
 var expressWs = require('express-ws')(app);
+var paused = false;
+
 app.engine(
   ".hbs",
   exphbs({
@@ -20,35 +23,42 @@ app.engine(
 
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "pages"));
+//set the template variables to be passed into home then into main.hbs 
 app.get("/", (request, response) => {
   response.render("home", {
-    name: array
+    calls: callsDict
   });
+
 });
+
 app.use(express.static(__dirname + "/images"));
 
+//post event handler
 app.post("/*", function(request, response) {
   console.log(JSON.stringify(request.body).replace(",", "<br>"));
   analytics = JSON.stringify(request.body)
     .split(",")
     .join("\n");
-  array.push(analytics);
+  n++;
+  callsDict[n] = analytics;
   response.send('OK');
-  web.send(analytics);
+  if (!paused){ //dont send update call to the front end if live update is paused
+    web.send("txt/" + analytics);
+  }
 });
 
 function setWS(ws){
   web = ws;
 }
 
-app.post("/trutv/xboxone/adobe", function(request, response) {
-  //console.log(response);
-  console.log(JSON.stringify(request.body).replace(",", "<br>"));
-  analytics = JSON.stringify(request.body)
-    .split(",")
-    .join("\n");
-  ws.send(analytics); //TODO need to add check that websocket is active
-});
+// app.post("/trutv/xboxone/adobe", function(request, response) {
+//   //console.log(response);
+//   console.log(JSON.stringify(request.body).replace(",", "<br>"));
+//   analytics = JSON.stringify(request.body)
+//     .split(",")
+//     .join("\n");
+// //TODO need to add check that websocket is active
+// });
 
 app.listen(port, err => {
   if (err) {
@@ -61,7 +71,11 @@ var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
-    console.log('received: %s', message)
+    console.log('received: %s', message);
+    if (message.includes('command')) {
+      parseCommand(message);
+    } 
+    
   })
   setWS(ws);
   /*
@@ -70,5 +84,15 @@ wss.on('connection', function (ws) {
     1000
   )
   */
-  
 })
+
+function parseCommand(cmnd){
+  var words = cmnd.split("/");
+  if (words[1] == 'select' && paused){
+    var analytics = callsDict[parseInt(words[2])];
+    web.send("txt/" + analytics);
+  }
+  if (words[1] == 'radio'){
+    paused = !paused; //paused the backend from sending live update to the front end
+  }
+}
